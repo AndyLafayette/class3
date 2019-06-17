312f4694285b1a015f00879be68d445b21780f1b
psyrj2 <psyrj2@nottingham.ac.uk>
2019-06-17 11:11:13 -0400

    first commit

---
+++ PageRankReducer.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+public class PageRankReducer
+extends Reducer<Text, Text, Text, Text> {
+@Override
+public void reduce(Text key, Iterable<Text> values, Context context)
+throws IOException, InterruptedException {
+int PR = 0;
+String val; 
+for (Text value : values) {
+String temp = value.toString();
+if (temp[temp.length() - 1] < '0' || temp[temp.length() - 1] > '9') {
+	val = temp;
+	continue;
+};
+String[] strs = temp.split(' ');
+PR += Integer.parseInt(strs[1]);
+}
+context.write(key, new Text(val + " " + PR));
+}
+}
