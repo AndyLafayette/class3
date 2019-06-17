ccf525f604b9a2fefd76ad0f7dbf5d002416446c
psyrj2 <psyrj2@nottingham.ac.uk>
2019-06-17 14:09:10 -0400

    2th commit

--- PageRankMapper.java
+++ PageRankMapper.java
@@ -11,12 +11,12 @@ public void map(LongWritable key, Text value, Context context)
 throws IOException, InterruptedException {
 String line = value.toString();
 String[] strs = line.split(" ");
-int length = strs.length();
+int length = strs.length;
 int outlinks = length - 2;
 double PR = Double.valueOf(strs[length - 1]); 
 for (int i = 0; i < outlinks; i++) {
 	context.write(new Text(strs[1 + i]), new Text(strs[0] + " " + PR/outlinks));
 }
-context.write(new Text(line.charAt(0)), new Text(line.substring(2, 3+(outlinks-1)*2)));
+context.write(new Text(line.charAt(0).toString()), new Text(line.substring(2, 3+(outlinks-1)*2)));
 }
 }
