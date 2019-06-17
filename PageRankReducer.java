0034343ecf5f5cf38555b5c85934b9c6676d02cd
psyrj2 <psyrj2@nottingham.ac.uk>
2019-06-17 15:55:30 -0400

    2th commit

--- PageRankReducer.java
+++ PageRankReducer.java
@@ -8,15 +8,15 @@ extends Reducer<Text, Text, Text, Text> {
 @Override
 public void reduce(Text key, Iterable<Text> values, Context context)
 throws IOException, InterruptedException {
-int PR = 0;
+int PR = 0; 
 String val; 
 for (Text value : values) {
 String temp = value.toString();
-if (temp[temp.length() - 1] < '0' || temp[temp.length() - 1] > '9') {
+if (temp.charAt(temp.length() - 1) < '0' || temp.charAt(temp.length() - 1) > '9') {
 	val = temp;
 	continue;
 };
-String[] strs = temp.split(' ');
+String[] strs = temp.split(" ");
 PR += Integer.parseInt(strs[1]);
 }
 context.write(key, new Text(val + " " + PR));
