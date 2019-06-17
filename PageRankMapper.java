f4e860fc63b258c918b1e9eff5b70df7a22df3d0
psyrj2 <psyrj2@nottingham.ac.uk>
2019-06-17 14:19:26 -0400

    2th commit

--- PageRankMapper.java
+++ PageRankMapper.java
@@ -16,9 +16,7 @@ int outlinks = length - 2;
 double PR = Double.valueOf(strs[length - 1]); 
 for (int i = 0; i < outlinks; i++) {
 	context.write(new Text(strs[1 + i]), new Text(strs[0] + " " + PR/outlinks));
-	System.out.println(strs[1 + i] + " " + strs[0] + " " + PR/outlinks);
 }
 context.write(new Text(Character.toString(line.charAt(0))), new Text(line.substring(2, 3+(outlinks-1)*2)));
-System.out.println(Character.toString(line.charAt(0)) + " " + line.substring(2, 3+(outlinks-1)*2));
 }
 }
